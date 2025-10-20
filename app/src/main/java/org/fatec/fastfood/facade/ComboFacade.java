d693d3d2b7c5096b39a9261c71e895f3b03421f5
Raphael Resende <raphaelresende10@gmail.com>
2025-10-20 20:26:15 -0300

    New: implementação da facade.

--- app/src/main/java/org/fatec/fastfood/facade/ComboFacade.java
+++ app/src/main/java/org/fatec/fastfood/facade/ComboFacade.java
@@ -4,7 +4,10 @@
  */
 
 package org.fatec.fastfood.facade;
+import org.fatec.fastfood.model.Bebida;
+import org.fatec.fastfood.model.Burger;
 import org.fatec.fastfood.model.Combo;
+import org.fatec.fastfood.model.Sobremesa;
 
 /**
  *
@@ -19,23 +22,40 @@ public class ComboFacade {
      * @param codigo número do combo (1, 2 ou 3)
      */
     public void criarCombo(int codigo) {
+        Burger burger;
+        Bebida bebida;
+        Sobremesa sobremesa;
+
         switch (codigo) {
             case 1:
-                // TODO: Criar Combo Master
                 // Big Cheddar (R$ 18.00) + Coca-Cola (R$ 8.00) + Sundae (R$ 9.90)
+                burger = new Burger("Big Cheddar", 18);
+                bebida = new Bebida("Coca-Cola", 8);
+                sobremesa = new Sobremesa("Sundae", 9);
+
                 break;
             case 2:
-                // TODO: Criar Super Combo
                 // Mega Bacon (R$ 16.00) + Guaraná (R$ 7.50) + Torta de Maçã (R$ 9.00)
+                burger = new Burger("Mega Bacon", 16);
+                bebida = new Bebida("Coca-Cola", 8);
+                sobremesa = new Sobremesa("Sundae", 9);
+            
                 break;
             case 3:
-                // TODO: Criar Combo Kids
                 // Mini Burger (R$ 12.00) + Suco Laranja (R$ 6.00) + Sorvete (R$ 4.90)
+                burger = new Burger("Mini Burger", 12);
+                bebida = new Bebida("Suco de Laranja", 6);
+                sobremesa = new Sobremesa("Sorvete", 4.9);
                 break;
             default:
                 System.out.println("Código de combo inválido!");
+                burger = null;
+                bebida = null;
+                sobremesa = null;
                 break;
         }
+
+        this.combo = new Combo(burger, bebida, sobremesa);
     }
     
     /**
@@ -48,9 +68,9 @@ public class ComboFacade {
         }
         
         System.out.println("\n=== Detalhes do Pedido ===");
-        // TODO: Exibir burger
-        // TODO: Exibir bebida
-        // TODO: Exibir sobremesa
+        System.out.println(combo.getBurger().toString());
+        System.out.println(combo.getBebida().toString());
+        System.out.println(combo.getSobremesa().toString());
         System.out.println("==========================");
     }
     
@@ -59,8 +79,7 @@ public class ComboFacade {
      * @return preço total
      */
     public double getPrecoTotal() {
-        // TODO: Retornar o preço total do combo
         // Dica: use combo.getPrecoTotal()
-        return 0.0; // SUBSTITUIR
+        return combo.getPrecoTotal(); // SUBSTITUIR
     }
 }
